eefe8755f94a4f662718584a9dd2741c9eb81f78
Dávid Ádám <david.adam@encosoft.hu>
2022-05-02 12:43:00 +0200

    uuid value added into product

--- src/network/mockApi/productApiMock.ts
+++ src/network/mockApi/productApiMock.ts
@@ -1,11 +1,14 @@
 import MockAdapter from 'axios-mock-adapter'
 import {Product} from '../../model/product'
 import axios from 'axios'
+import 'react-native-get-random-values'
+import {v4} from 'uuid'
 
-const mock = new MockAdapter(axios)
+const mock = new MockAdapter(axios, {delayResponse: 750})
 
 mock.onGet('read').reply<Product[]>(200, [
   {
+    id: v4(),
     name: "Harry Potter and the Philosopher's Stone",
     description:
       "When a letter arrives for unhappy but ordinary Harry Potter, a decade-old secret is revealed to him that apparently he's the last to know. His parents were wizards, killed by a Dark Lord's curse when Harry was just a baby, and which he somehow survived. Leaving his unsympathetic aunt and uncle for Hogwarts, a wizarding school brimming with ghosts and enchantments, Harry stumbles upon a sinister mystery when he finds a three-headed dog guarding a room on the third floor. Then he hears of a missing stone with astonishing powers which could be valuable, dangerous - or both. An incredible adventure is about to begin! These new editions of the classic and internationally bestselling, multi-award-winning series feature instantly pick-up-able new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It's time to PASS THE MAGIC ON .",
